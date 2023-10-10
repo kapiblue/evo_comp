34210c0a2df3873ff71a8d29512b759502d9671b
kapiblue <kapiblue@outlook.com>
2023-10-10 23:08:09 +0200

    feat: csv reading and storing int values

--- lab1/distance_matrix_creator.cpp
+++ lab1/distance_matrix_creator.cpp
@@ -1,23 +1,89 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 
-int main()
+class DistanceMatrixCreator
 {
-    ifstream file;
-    file.open("TSPA.csv");
+private:
+    vector<vector<int>> distance_matrix;
 
-    string line;
-    getline(file, line);
-
-    while (getline(file, line))
+public:
+    void read_coordinates(string filename, vector<int> *xs,
+                          vector<int> *ys, vector<int> *costs)
     {
-        cout << line << endl;
+
+        // Create a file pointer
+        ifstream file;
+
+        // Open csv file
+        file.open(filename);
+
+        if (!file.is_open())
+        {
+            throw runtime_error("Could not open the file");
+        }
+
+        // For temporary storage
+        vector<int> row;
+        string line, word;
+
+        while (file.good())
+        {
+            row.clear();
+
+            // Read one row and store
+            // and store it in line
+            getline(file, line);
+
+            // For breaking words
+            stringstream s(line);
+
+            while (getline(s, word, ';'))
+            {
+                row.push_back(stoi(word));
+            }
+
+            (*xs).push_back(row[0]);
+            (*ys).push_back(row[1]);
+            (*costs).push_back(row[2]);
+        }
+
+        file.close();
     }
 
-    file.close();
+    void create(string filename)
+    {
+
+        vector<int> xs, ys, costs;
+
+        read_coordinates(filename, &xs, &ys, &costs);
+
+        for (int x : xs)
+        {
+            cout << x << endl;
+        }
+    }
+
+    vector<vector<int>> get_distance_matrix()
+    {
+        return distance_matrix;
+    }
+};
+
+int main()
+{
+
+    DistanceMatrixCreator dmc;
+
+    dmc.create("TSPA.csv");
+
+    vector<vector<int>> dm;
+    dm = dmc.get_distance_matrix();
 
     return 0;
 }
