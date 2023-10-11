297389651466ebabefe2e248c4bdca5b1d186edd
kapiblue <kapiblue@outlook.com>
2023-10-11 12:50:45 +0200

    feat: distance matrix computation

--- lab1/distance_matrix_creator.cpp
+++ lab1/distance_matrix_creator.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <string>
+#include <cmath> // power and sqrt
 #include <fstream>
+#include <iostream>
 #include <sstream>
-#include <vector>
+#include <string>
 #include <tuple>
-#include<cmath> // power and sqrt
+#include <vector>
 
 using namespace std;
 
@@ -14,10 +14,11 @@ private:
     vector<vector<int> > distance_matrix;
 
 public:
-    void read_coordinates(string filename, vector<int> *xs,
-                          vector<int> *ys, vector<int> *costs)
+    void read_coordinates(string filename,
+                          vector<int> *xs,
+                          vector<int> *ys,
+                          vector<int> *costs)
     {
-
         // Create a file pointer
         ifstream file;
 
@@ -46,12 +47,16 @@ public:
 
             while (getline(s, word, ';'))
             {
+               
                 row.push_back(stoi(word));
             }
 
+            if (row.size() > 0)
+                {
             (*xs).push_back(row[0]);
             (*ys).push_back(row[1]);
             (*costs).push_back(row[2]);
+                }
         }
 
         file.close();
@@ -59,7 +64,6 @@ public:
 
     void create(string filename)
     {
-
         vector<int> xs, ys, costs;
 
         read_coordinates(filename, &xs, &ys, &costs);
@@ -68,26 +72,45 @@ public:
         {
             cout << x << endl;
         }
+
+        for (int i = 0; i < xs.size(); i++)
+        {
+            vector<int> temp;
+            for (int j = 0; j < xs.size(); j++)
+            {
+                double ed = euclidean_distance(xs[i], ys[i], xs[j], ys[j]);
+                temp.push_back(round(ed));
+            }
+            distance_matrix.push_back(temp);
+        }
     }
 
-    double euclidean_distance(int x1, int y1, int x2, int y2){
-
+    double euclidean_distance(int x1, int y1, int x2, int y2)
+    {
         double euc_dist;
         euc_dist = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 
         return euc_dist;
-
     }
 
-    vector<vector<int> > get_distance_matrix()
+    vector<vector<int> > get_distance_matrix() { return distance_matrix; }
+
+    void print_distance_matrix()
     {
-        return distance_matrix;
+        for (int i = 0; i < distance_matrix.size(); ++i)
+        {
+            for (int j = 0; j < distance_matrix[i].size(); ++j)
+            {
+                cout << distance_matrix[i][j] << " ";
+                cout << i << " " << j << endl;
+            }
+            cout << endl;
+        }
     }
 };
 
 int main()
 {
-
     DistanceMatrixCreator dmc;
 
     dmc.create("TSPA.csv");
@@ -98,8 +121,7 @@ int main()
     double ed;
     ed = dmc.euclidean_distance(1, 1, 3, 3);
 
-    cout << ed << endl;
-    cout << round(ed) << endl;
+    dmc.print_distance_matrix();
 
     return 0;
 }
