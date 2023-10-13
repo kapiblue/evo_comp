04e639ce93c433cb0ba284585a5dc2f9e1775d19
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-10-13 16:07:09 +0200

    updated plotting draft

--- plotting/make_plot.py
+++ plotting/make_plot.py
@@ -2,19 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-nodes = pd.read_csv('lab1/TSPD.csv', sep=';', header=None).to_numpy()
-xs = nodes[:,0]
-ys = nodes[:,1]
-costs = nodes[:,2]
+instancePath = "../lab1/TSPA.csv"
+edgesPath = "./edges.csv"
 
-tmp1 = pd.read_csv('plotting/file.csv', sep=';', header=None).to_numpy().squeeze()
-tmp2 = np.roll(tmp1, 1)
-edges = np.stack([tmp1, tmp2]).T
+# Read raw Instance
+instance = pd.read_csv(instancePath, sep=';', header=None).T.to_numpy()
+xs, ys, costs = instance
+# Read generated edges
+edges = pd.read_csv(edgesPath, sep=';', header=None).to_numpy()
 
-plt.scatter(xs, ys, s=costs/20)
+colors = ['red', 'green', 'blue', 'black']
+fig, axs = plt.subplots(2, 2, figsize=(20, 10), dpi = 200)
+fig.suptitle('Alg 1', fontsize=20)
 
-for idx1, idx2 in edges:
-    plt.plot([xs[idx1], xs[idx2]], [ys[idx1], ys[idx2]], color='red')
-
-
-plt.savefig('plotting/plot.jpg')
+for i, ax in enumerate(axs.flat):
+    # Plot coords
+    ax.scatter(xs, ys, s=costs/10)
+    # Plot edges
+    for idx1, idx2 in edges:
+        ax.plot([xs[idx1], xs[idx2]], [ys[idx1], ys[idx2]], 
+                 color=colors[i])
+    
+    ax.set_title(f"Instance {i}")
+    ax.set(xlabel='x', ylabel='y')
+    ax.label_outer()
+# Save
+fig.savefig('plot.jpg')
