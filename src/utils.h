01b4fb266430082d3dfe463948591121c74b0c7c
kapiblue <kapiblue@outlook.com>
2023-10-17 22:29:31 +0200

    add descending argsort

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
-#include <numeric>
+#include <numeric>   // iota
+#include <algorithm> // sort
 
 #ifndef _UTILS_H
 #define _UTILS_H
@@ -12,9 +13,12 @@ void read_csv(std::string filename,
 double euclidean_distance(int x1, int y1, int x2, int y2);
 
 template <typename T>
-T contain(std::vector<T> vec, T value){
-    for(int i=0; i<vec.size(); i++){
-        if(vec[i] == value){
+T contain(std::vector<T> vec, T value)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] == value)
+        {
             return true;
         }
     }
@@ -33,6 +37,23 @@ double mean(std::vector<T> vec)
     {
         return std::reduce(vec.begin(), vec.end(), 0.0) / vec.size();
     }
+}
+
+// Returns the indices that would sort an array.
+// Descending order
+template <typename T>
+std::vector<size_t> argsort(const std::vector<T> &v)
+{
+    // get inital indexes
+    std::vector<size_t> idx(v.size());
+    iota(idx.begin(), idx.end(), 0);
+
+    // Sort indexes based on values of vector
+    std::stable_sort(idx.begin(), idx.end(),
+                     [&v](size_t i1, size_t i2)
+                     { return v[i1] > v[i2]; });
+
+    return idx;
 }
 
 #endif
