6f2f173c9fe87eb06f18e5e1fc770fae6df21683
kapiblue <kapiblue@outlook.com>
2023-11-08 16:31:43 +0100

    working intra edges candidates

--- src/cm_local_search_solver.cpp
+++ src/cm_local_search_solver.cpp
@@ -9,6 +9,7 @@
 #include <random>
 #include <numeric>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 using namespace N;
@@ -17,27 +18,27 @@ CMLocalSearchSolver::CMLocalSearchSolver(string instance_filename, double fracti
     : LocalSearchSolver(instance_filename, fraction_nodes, initial_solution)
 {
     this->construct_candidate_nodes();
+    this->construct_node_idxs_lookup();
 }
 
 void CMLocalSearchSolver::run_candidates(string neigh_method, string search_method)
 {
     int current_best_delta = -1;
-    int best_inter_delta, best_intra_nodes_delta, best_intra_edges_delta;
+    int best_inter_delta, best_intra_edges_delta;
 
     int arg1, arg2, temp_arg1, temp_arg2;
     string move_type;
 
     while (current_best_delta < 0)
     {
-        this->find_best_neighbor_edges_extended(&best_inter_delta, &temp_arg1, &temp_arg2, search_method);
-        break;
-        if (best_inter_delta < current_best_delta)
+        current_best_delta = 0;
+        this->find_best_neighbor_edges_from_candidates(&best_intra_edges_delta, &temp_arg1, &temp_arg2, search_method);
+        if (best_intra_edges_delta < current_best_delta)
         {
             arg1 = temp_arg1;
             arg2 = temp_arg2;
-            move_type = "edges_extended";
-            current_best_delta = best_inter_delta;
-
+            move_type = "intra_edges";
+            current_best_delta = best_intra_edges_delta;
         }
 
         if (current_best_delta >= 0)
@@ -46,57 +47,92 @@ void CMLocalSearchSolver::run_candidates(string neigh_method, string search_meth
             // if the new current delta is not less than 0
             break;
         }
-        this->best_sol_evaluation += current_best_delta;
-        apply_move(move_type, &arg1, &arg2);
         // cout << "Move type " << move_type << " Delta " << current_best_delta << endl;
         // cout << arg1 << " " << arg2 << endl;
+        this->apply_move(move_type, arg1, arg2);
+        this->best_sol_evaluation += current_best_delta;
         // cout << this->best_sol_evaluation << endl;
+        // this->best_solution.print();
+        // cout << this->best_solution.evaluate(&this->dist_mat, &this->costs) << endl;
     }
     // int eval = best_solution.evaluate(&this->dist_mat, &this->costs);
     // cout << "Actual evaluation: " << eval << endl;
     // this->best_solution.print();
 }
 
-void CMLocalSearchSolver::find_best_neighbor_edges_extended(int *out_delta, int *first_edge_idx, int *second_edge_idx, string search_method)
+void CMLocalSearchSolver::find_best_neighbor_edges_from_candidates(int *out_delta, int *first_edge_idx,
+                                                                   int *second_edge_idx, string search_method)
 {
     // We assume edge 0 to connect nodes[0] with nodes[1]
     // Last edge is between last node and the first node
-    int edges_number = this->best_solution.get_number_of_nodes();
     int min_delta = 0;
     int min_edge1_idx = -1;
     int min_edge2_idx = -1;
-    int delta;
-
-    if (search_method == "GREEDY")
-    {
-        shuffle(this->iterator1.begin(), this->iterator1.end(), this->rd);
-        shuffle(this->candidate_nodes.begin(), this->candidate_nodes.end(), this->rd);
-    }
 
     for (auto &edge1_idx : this->iterator1)
     {
-        for (auto &edge2_idx : this->candidate_nodes[edge1_idx])
+        for (auto &cand_node : this->candidate_nodes[edge1_idx])
         {
+            if (this->best_solution.contains(cand_node))
+            {
+                int cand_idx = this->get_solution_index(cand_node);
+                int delta_next = this->best_solution.calculate_delta_intra_route_edges(
+                    &this->dist_mat, edge1_idx, cand_idx);
+                int edge1_prev_idx = this->best_solution.get_prev_node_idx(edge1_idx);
+                int cand_prev_idx = this->best_solution.get_prev_node_idx(cand_idx);
+                int delta_prev = this->best_solution.calculate_delta_intra_route_edges(
+                    &this->dist_mat, edge1_prev_idx, cand_prev_idx);
+                if (delta_next < min_delta && edge1_idx < cand_idx - 1)
+                {
+                    min_delta = delta_next;
+                    min_edge1_idx = edge1_idx;
+                    min_edge2_idx = cand_idx;
+                }
+                else if (delta_prev < min_delta && edge1_prev_idx < cand_prev_idx - 1)
+                {
+                    min_delta = delta_prev;
+                    min_edge1_idx = edge1_prev_idx;
+                    min_edge2_idx = cand_prev_idx;
+                }
+            }
         }
     }
+    // cout << "MIN DELTA" << min_delta << endl;
+    // cout << min_delta << " " << min_edge1_idx << " " << min_edge2_idx << endl;
     *out_delta = min_delta;
     *first_edge_idx = min_edge1_idx;
     *second_edge_idx = min_edge2_idx;
 }
 
-void CMLocalSearchSolver::construct_candidate_nodes(){
+void CMLocalSearchSolver::apply_move(string move_type, int arg1, int arg2)
+{
+
+    if (move_type == "inter")
+    {
+        this->best_solution.exchange_node_at_idx(arg1, arg2);
+    }
+    else if (move_type == "intra_edges")
+    {
+        this->best_solution.exchange_2_edges(arg1, arg2);
+        // update_node_lookup_edges();
+    }
+}
+
+void CMLocalSearchSolver::construct_candidate_nodes()
+{
     vector<vector<int>> dist_mat = this->get_distance_matrix();
     vector<int> costs = this->get_costs();
-    for(int node_idx=0; node_idx<this->total_nodes; node_idx++){
+    for (int node_idx = 0; node_idx < this->total_nodes; node_idx++)
+    {
         vector<int> single_node_candidates(this->total_nodes);
         iota(single_node_candidates.begin(), single_node_candidates.end(), 0);
         single_node_candidates.erase(single_node_candidates.begin() + node_idx);
 
-        sort(single_node_candidates.begin(), single_node_candidates.end(), [&](int a, int b){
+        sort(single_node_candidates.begin(), single_node_candidates.end(), [&](int a, int b)
+             {
             int a_value = dist_mat[node_idx][a] + costs[a];
             int b_value = dist_mat[node_idx][b] + costs[b];
-            return a_value<b_value;
-        });
+            return a_value<b_value; });
 
         single_node_candidates = {single_node_candidates.begin(), single_node_candidates.begin() + 10};
         this->candidate_nodes.push_back(single_node_candidates);
@@ -107,4 +143,31 @@ void CMLocalSearchSolver::construct_candidate_nodes(){
     //     }
     //     cout<<endl;
     // }
+}
+
+/*
+Constructs a node lookup structure for fast verification
+at what index the node is in the solution
+*/
+void CMLocalSearchSolver::construct_node_idxs_lookup()
+{
+    map<int, int> node_lookup;
+    vector<int> nodes = this->best_solution.get_nodes();
+    for (int i = 0; i < this->best_solution.get_number_of_nodes(); ++i)
+    {
+        node_lookup[nodes[i]] = i;
+    }
+    this->node_lookup = node_lookup;
+}
+
+int CMLocalSearchSolver::get_solution_index(int node)
+{
+    vector<int> nodes = this->best_solution.get_nodes();
+    for (int i = 0; i < this->best_solution.get_number_of_nodes(); ++i)
+    {
+        if (nodes[i] == node)
+        {
+            return i;
+        }
+    }
 }
--- src/cm_local_search_solver.h
+++ src/cm_local_search_solver.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <map>
 
 #include "solution.h"
 #include "local_search_solver.h"
@@ -12,13 +13,18 @@ namespace N
     {
     private:
         std::vector<std::vector<int>> candidate_nodes;
+        // For fast access of node indices
+        std::map<int, int> node_lookup;
 
     public:
         CMLocalSearchSolver(std::string instance_filename, double fraction_nodes, Solution initial_solution);
         void run_candidates(std::string neigh_method, std::string search_method);
         void construct_candidate_nodes();
-        void find_best_neighbor_edges_extended(int *out_delta, int *first_edge_idx,
-                                               int *second_edge_idx, std::string search_method);
+        void construct_node_idxs_lookup();
+        void find_best_neighbor_edges_from_candidates(int *out_delta, int *first_edge_idx,
+                                                      int *second_edge_idx, std::string search_method);
+        void apply_move(std::string move_type, int arg1, int arg2);
+        int get_solution_index(int node);
     };
 
 }
